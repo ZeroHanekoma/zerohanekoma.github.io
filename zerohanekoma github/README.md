bc0111db62dc426248d40e0ff8de815794dc6857
ZeroHanekoma <67618133+ZeroHanekoma@users.noreply.github.com>
2022-07-04 16:54:06 -0700

    Add files via upload

---
+++ zerohanekoma github/README.md
@@ -0,0 +1,12 @@
+# Minimal Startpage
+<p align="center">
+  <a href="https://github.com/pedro-pablo/minimal-startpage-template/blob/master/LICENSE" alt="License">
+    <img src="https://img.shields.io/github/license/pedro-pablo/minimal-startpage-template.svg" /></a>
+  <img src="https://img.shields.io/github/repo-size/pedro-pablo/minimal-startpage-template.svg" alt="Repo size" />
+</p>
+<p>
+
+my personal startpage, ripped and frankensteined together from a variety of <a href="https://startpages.github.io">startpages</a>.
+
+
+</p>
---
+++ zerohanekoma github/js/quotes.js
@@ -0,0 +1,262 @@
+/* ======================================================/
+MY MASSIVE LIST OF SMARMY 2DEEP4U QUOTES
+
+
+copy this from the end of this line to add new quotes.
+		{
+            "quote": "Template",
+            "author": "Default"
+        },
+		
+feel smarter than all your friends!
+
+/====================================================== */
+
+
+
+const quotes = [
+        {
+            "quote": "The Bible tells us to be like God, and then on page after page it describes God as a mass murderer. This may be the single most important key to the political behavior of Western Civilization.",
+            "author": "Robert Anton Wilson"
+        },
+        {
+            "quote": "Anyone in the United States today who isn't paranoid must be crazy.",
+            "author": "Robert Anton Wilson"
+        },
+        {
+            "quote": "Arguing that you don't care about the right to privacy because you have nothing to hide is no different than saying you don't care about free speech because you have nothing to say.",
+            "author": "Edward Snowden"
+        },
+        {
+            "quote": "Only the madman is absolutely sure.",
+            "author": "Robert Anton Wilson"
+        },
+        {
+            "quote": "Human beings live in their myths. They only endure their realities.",
+            "author": "Robert Anton Wilson"
+        },
+        {
+            "quote": "The basic tool for the manipulation of reality is the manipulation of words. If you can control the meaning of words, you can control the people who must use the words.",
+            "author": "Philip K. Dick"
+        },
+        {
+            "quote": "Reality is that which, when you stop believing in it, doesn't go away.",
+            "author": "Philip K. Dick"
+        },
+        {
+            "quote": "You will be required to do wrong no matter where you go. It is the basic condition of life, to be required to violate your own identity.",
+            "author": "Philip K. Dick"
+        },
+        {
+            "quote": "Do you think that when we die and appear before God on Judgment Day, that our sins will be listed in chronological order on in order of severity, which could be ascending or descending, or alphabetically?",
+            "author": "Philip K. Dick"
+        },
+        {
+            "quote": "The government should be afraid of the people, the people shouldn't be afraid of the government.",
+            "author": "Edward Snowden"
+        },
+        {
+            "quote": "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.",
+            "author": "Terry Pratchett"
+        },
+        {
+            "quote": "In the beginning there was nothing, which exploded.",
+            "author": "Terry Pratchett"
+        },
+        {
+            "quote": "HUMANS NEED FANTASY TO BE HUMAN. TO BE THE PLACE WHERE THE FALLING ANGEL MEETS THE RISING APE.",
+            "author": "Terry Pratchett"
+        },
+        {
+            "quote": "Evil begins when you begin to treat people as things.",
+            "author": "Terry Pratchett"
+        },
+        {
+            "quote": "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
+            "author": "J.R.R. Tolkien"
+        },
+        {
+            "quote": "Do you wish me a good morning, or mean that it is a good morning whether I want it or not; or that you feel good this morning; or that it is a morning to be good on?",
+            "author": "J.R.R. Tolkien"
+        },
+        {
+            "quote": "All we have to decide is what to do with the time that is given us.",
+            "author": "J.R.R. Tolkien"
+        },
+        {
+            "quote": "Some day you will be old enough to start reading fairy tales again.",
+            "author": "C.S. Lewis "
+        },
+        {
+            "quote": "Crying is all right in its way while it lasts. But you have to stop sooner or later, and then you still have to decide what to do.",
+            "author": "C.S. Lewis "
+        },
+        {
+            "quote": "What you see and what you hear depends a great deal on where you are standing. It also depends on what sort of person you are.",
+            "author": "C.S. Lewis "
+        },
+        {
+            "quote": "He shall fall down into a pit called Because, and there he shall perish with the dogs of reason.",
+            "author": "Aleister Crowley"
+        },
+        {
+            "quote": "Life is pleasant. Death is peaceful. It's the transition that's troublesome.",
+            "author": "Isaac Asimov"
+        },
+        {
+            "quote": "It has become appallingly obvious that our technology has exceeded our humanity.",
+            "author": "Albert Einstein"
+        },
+        {
+            "quote": "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+            "author": "Douglas Adams"
+        },
+        {
+            "quote": "Nothing is constant but change.",
+            "author": "Sakyamuni, founder of Buddhism"
+        },
+        {
+            "quote": "May god stand between you and harm, in all the dark places you must walk.",
+            "author": "Ancient Egyptian Proverb"
+        },
+        {
+            "quote": "To win one hundred victories in one hundred battles is not the highest skill. To subdue the enemy without fighting, is the highest skill.",
+            "author": "Sun-Tsu"
+        },
+        {
+            "quote": "What is now proved was once only imagined.",
+            "author": "William Blake"
+        },
+        {
+            "quote": "And if you gaze for long into an abyss, the abyss gazes also into you.",
+            "author": "Friedrich Nietzsche"
+        },
+        {
+            "quote": "The journey is the reward.",
+            "author": "Taoist Saying"
+        },
+        {
+            "quote": "Reality is that part of the imagination we all agree on.",
+            "author": "Unknown"
+        },
+        {
+            "quote": "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+            "author": "Albert Einstein"
+        },
+        {
+            "quote": "Any sufficiently advanced technology is indistinguishable from magic.",
+            "author": "Arthur C. Clarke"
+        },
+        {
+            "quote": "I am the wisest man alive, for I know one thing, and that is that I know nothing.",
+            "author": "Socrates"
+        },
+        {
+            "quote": "To alcohol! The cause of, and solution to, all of life's problems.",
+            "author": "Homer J. Simpson"
+        },
+        {
+            "quote": "You're in pretty good shape for the shape you are in.",
+            "author": "Dr. Seuss"
+        },
+        {
+            "quote": "I like nonsense; it wakes up the brain cells.",
+            "author": "Dr. Seuss"
+        },
+        {
+            "quote": "From there to here, and here to there, funny things are everywhere.",
+            "author": "Dr. Seuss"
+        },
+        {
+            "quote": "Unless someone like you cares a whole awful lot, nothing is going to get better. It's not.",
+            "author": "Dr. Seuss"
+        },
+        {
+            "quote": "Don't cry because it's over. Smile because it happened.",
+            "author": "Dr. Seuss"
+        },
+        {
+            "quote": "Everything is theoretically impossible, until it is done.",
+            "author": "Robert A. Heinlein"
+        },
+        {
+            "quote": "The scientists of today think deeply instead of clearly. One must be sane to think clearly, but one can think deeply and be quite insane.",
+            "author": "Nikola Tesla"
+        },
+        {
+            "quote": "Three things are necessary for the salvation of man: to know what he ought to believe; to know what he ought to desire; and to know what he ought to do.",
+            "author": "Thomas Aquinas"
+        },
+        {
+            "quote": "Great things are done by a series of small things brought together.",
+            "author": "Vincent Van Gogh"
+        },
+        {
+            "quote": "It is difficult to free fools from the chains they revere.",
+            "author": "Voltaire"
+        },
+        {
+            "quote": "Judge a man by his questions rather than his answers.",
+            "author": "Voltaire"
+        },
+        {
+            "quote": "It is better to risk saving a guilty man than to condemn an innocent one.",
+            "author": "Voltaire"
+        },
+        {
+            "quote": "Man is free at the moment he wishes to be.",
+            "author": "Voltaire"
+        },
+        {
+            "quote": "If the freedom of speech is taken away then dumb and silent we may be led, like sheep to the slaughter.",
+            "author": "George Washington"
+        },
+        {
+            "quote": "Freedom is never voluntarily given by the oppressor; it must be demanded by the oppressed.",
+            "author": "Martin Luther King, Jr."
+        },
+        {
+            "quote": "A hero is someone who understands the responsibility that comes with his freedom.",
+            "author": "Bob Dylan"
+        },
+        {
+            "quote": "Think for yourself and question authority.",
+            "author": "Timothy Leary"
+        },
+        {
+            "quote": "We are dealing with the best-educated generation in history. But they've got a brain dressed up with nowhere to go.",
+            "author": "Timothy Leary"
+        },
+        {
+            "quote": "The role of anxiety in the development of human personality is central, and it is intricate beyond our understanding.",
+            "author": "Timothy Leary"
+        },
+        {
+            "quote": "If you don't like what you're doing, you can always pick up your needle and move to another groove.",
+            "author": "Timothy Leary"
+        },
+        {
+            "quote": "The only way to make sense out of change is to plunge into it, move with it, and join the dance.",
+            "author": "Alan Watts"
+        },
+        {
+            "quote": "To have faith is to trust yourself to the water. When you swim you don't grab hold of the water, because if you do you will sink and drown. Instead you relax, and float.",
+            "author": "Alan Watts"
+        },
+        {
+            "quote": "Inside every cynical person, there is a disappointed idealist.",
+            "author": "George Carlin"
+        },
+        {
+            "quote": "Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?",
+            "author": "George Carlin"
+        },
+        {
+            "quote": "You're only given a little spark of madness. You mustn't lose it.",
+            "author": "Robin Williams"
+        },
+        {
+            "quote": "If there is a God, his plan is very similar to someone not having a plan.",
+            "author": "Eddie Izzard"
+        },
+];
