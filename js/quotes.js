632196334a258249ac1d7821197f9718376cfa0c
ZeroHanekoma <67618133+ZeroHanekoma@users.noreply.github.com>
2022-07-04 18:57:29 -0700

    Update quotes.js
    added an insane amount of quotes.  Thanks AZQuote.

--- js/quotes.js
+++ js/quotes.js
@@ -10,6 +10,9 @@ copy this from the end of this line to add new quotes.
 		
 feel smarter than all your friends!
 
+Thanks to the anon on /wg/ who pointed out how 
+retardedly easy this is to setup.
+
 /====================================================== */
 
 
@@ -259,4 +262,429 @@ const quotes = [
             "quote": "If there is a God, his plan is very similar to someone not having a plan.",
             "author": "Eddie Izzard"
         },
+		{
+            "quote": "Nothing good will happen in my life, and I could see it as a blessing",
+            "author": "Sugaru Miaki"
+        },
+		{
+            "quote": "In eternity there is no time, only an instant long enough for a joke.",
+            "author": "Hermann Hesse"
+        },
+		{
+            "quote": "The cup was emptied and would never be filled again.",
+            "author": "Hermann Hesse"
+        },
+		{
+            "quote": "Do unto others as they do unto you.",
+            "author": "Anton Szandor LaVey"
+        },
+		{
+            "quote": "There are many who would like my time. I shun them. There are some who share my time. I am entertained by them. There are precious few who contribute to my time. I cherish them.",
+            "author": "Anton Szandor LaVey"
+        },
+		{
+            "quote": "What good are friends if they cant do you any good?",
+            "author": "Anton Szandor LaVey"
+        },
+		{
+            "quote": "If you want to find out who your real friends are, sink the ship. The first ones to jump aren't your friends.",
+            "author": "Marilyn Manson"
+        },
+		{
+            "quote": "We live in a society of victimization, where people are much more comfortable being victimized than actually standing up for themselves.",
+            "author": "Marilyn Manson"
+        },
+		{
+            "quote": "Find what you are afraid of, face it, and then you won't be afraid of it anymore.",
+            "author": "Marilyn Manson"
+        },
+		{
+            "quote": "You get depressed because you know that you're not what you should be.",
+            "author": "Marilyn Manson"
+        },
+		{
+            "quote": "The only man who never makes mistakes is the man who never does anything.",
+            "author": "Theodore “Teddy” Roosevelt"
+        },
+		{
+            "quote": "Knowing what's right doesn't mean much unless you do what's right.",
+            "author": "Theodore “Teddy” Roosevelt"
+        },
+		{
+            "quote": "Speak softly and carry a big stick; you will go far.",
+            "author": "Theodore “Teddy” Roosevelt"
+        },
+		{
+            "quote": "People ask the difference between a leader and a boss. The leader leads, and the boss drives",
+            "author": "Theodore “Teddy” Roosevelt"
+        },
+		{
+            "quote": "If I must choose between peace and righteousness, I choose righteousness.",
+            "author": "Theodore \"Teddy\" Roosevelt"
+        },
+		{
+            "quote": "Those with less curiosity or ambition just mumble that God works in mysterious ways. I intend to catch him in the act.",
+            "author": "Damien Echols"
+        },
+		{
+            "quote": "Nostalgia is the only friend that stays with you forever.",
+            "author": "Damien Echols"
+        },
+		{
+            "quote": "How do you make someone understand what it means or how it feels to be torn in half? Not many people know this desperate need to be put back together again.",
+            "author": "Damien Echols"
+        },
+		{
+            "quote": "Good things are always coming; sometimes we just forget it.",
+            "author": "Damien Echols"
+        },
+		{
+            "quote": "The world is changing so quickly, it's hard to get anything right for long.",
+            "author": "Christopher \"moot\" Poole"
+        },
+		{
+            "quote": "The truth may be stretched thin, but it never breaks, and it always surfaces above lies, as oil floats on water.",
+            "author": "Miguel de Cervantes Saavedra"
+        },
+		{
+            "quote": "pls rember that wen u feel scare or frigten, never forget ttimes wen u feeled happy. wen day is dark alway rember happy day",
+            "author": "Anonymous, /jp/"
+        },
+		{
+            "quote": "The world breaks everyone, and afterward, many are strong at the broken places.",
+            "author": "Ernest Hemingway"
+        },
+		{
+            "quote": "When people talk, listen completely. Most people never listen.",
+            "author": "Ernest Hemingway"
+        },
+		{
+            "quote": "You can't get away from yourself by moving from one place to another.",
+            "author": "Ernest Hemingway"
+        },
+		{
+            "quote": "But I am a blasted tree; the bolt has entered my soul; and I felt then that I should survive to exhibit, what I shall soon cease to be - a miserable spectacle of wrecked humanity, pitiable to others, and intolerable to myself",
+            "author": "Mary Shelley"
+        },
+		{
+            "quote": "That is not dead which can eternal lie, and with strange aeons even death may die.",
+            "author": "H.P. Lovecraft"
+        },
+		{
+            "quote": "Because someone asked, of course. We are obliged to aid where we can, for otherwise we are naught but philistines. Also, we expand our knowledgebase, for example by finding out how many spiders can fit inside a drow's vagina.",
+            "author": "Anonymous, /tg/"
+        },
+		{
+            "quote": "All statements are true in some sense, false in some sense, meaningless in some sense, true and false in some sense, true and meaningless in some sense, false and meaningless in some sense, and true and false and meaningless in some sense.",
+            "author": "Malaclypse the Younger"
+        },
+		{
+            "quote": "One cannot shape the world without being reshaped in the process. Each gain of power requires its own sacrifice.",
+            "author": "Phil Hine"
+        },
+		{
+            "quote": "A Discordian is someone who sees windmills and thinks they might be giants",
+            "author": "Robert Anton Wilson"
+        },
+		{
+            "quote": "Everyone carries a piece of the puzzle. Nobody comes into your life by mere coincidence. Trust your instincts. Do the unexpected. Find the others.",
+            "author": "Timothy Leary"
+        },
+		{
+            "quote": "People have to go out of their mind before they can come to their senses.",
+            "author": "Timothy Leary"
+        },
+		{
+            "quote": "A man without justice is a beast, and a man who would make himself a beast forgets the pain of being a man.",
+            "author": "Timothy Leary"
+        },
+		{
+            "quote": "What we call reality is in fact nothing more than a culturally sanctioned and linguistically reinforced hallucination.",
+            "author": "Terence McKenna"
+        },
+		{
+            "quote": "You are a divine being. You matter, you count. You come from realms of unimaginable power and light, and you will return to those realms.",
+            "author": "Terence McKenna"
+        },
+		{
+            "quote": "If you don't have a plan, you become part of somebody else's plan.",
+            "author": "Terence McKenna"
+        },
+		{
+            "quote": "What we imagine is order is merely the prevailing form of chaos.",
+            "author": "Kerry Thornley"
+        },
+		{
+            "quote": "We must all face the fact that our leaders are certifiably insane or worse",
+            "author": "William S. Burroughs"
+        },
+		{
+            "quote": "After one look at this planet any visitor from outer space would say \'I want to see the manager.\'",
+            "author": "William S. Burroughs"
+        },
+		{
+            "quote": "Your mind will answer most questions if you learn to relax and wait for the answer.",
+            "author": "William S. Burroughs"
+        },
+		{
+            "quote": "Open your mind and let the pictures out",
+            "author": "William S. Burroughs"
+        },
+		{
+            "quote": "I think anybody incapable of changing his mind is crazy.",
+            "author": "William S. Burroughs"
+        },
+		{
+            "quote": "The downward spiral of Dumbness in America is about to hit a new low.",
+            "author": "Hunter S. Thompson"
+        },
+		{
+            "quote": "Life has become immeasurably better since I have been forced to stop taking it seriously.",
+            "author": "Hunter S. Thompson"
+        },
+		{
+            "quote": "I learned a long time ago that reality was much weirder than anyone's imagination.",
+            "author": "Hunter S. Thompson"
+        },
+		{
+            "quote": "When the going gets weird, the weird turn pro.",
+            "author": "Hunter S. Thompson"
+        },
+		{
+            "quote": "I've given up asking questions. l merely float on a tsunami of acceptance of anything life throws at me ... and marvel stupidly.",
+            "author": "Terry Gilliam"
+        },
+		{
+            "quote": "Some people are passionate about aisles, others about window seats.",
+            "author": "Terry Jones"
+        },
+		{
+            "quote": "People can tell the truth much more freely when they're apparently lying.",
+            "author": "Eric Idle"
+        },
+		{
+            "quote": "Life has a very simple plot: first you're here and then you're not.",
+            "author": "Eric Idle"
+        },
+		{
+            "quote": "Sticks and stones may break my bones, but words will make me go in a corner and cry by myself for hours.",
+            "author": "Eric Idle"
+        },
+		{
+            "quote": "The sad thing about true stupidity is that you can do absolutely nothing about it.",
+            "author": "John Cleese"
+        },
+		{
+            "quote": "Life is a terminal disease, and it is sexually transmitted.",
+            "author": "John Cleese"
+        },
+		{
+            "quote": "A wonderful thing about true laughter is that it just destroys any kind of system of dividing people.",
+            "author": "John Cleese"
+        },
+		{
+            "quote": "We come from nothing, we are going back to nothing-In the end what have we lost? Nothing!",
+            "author": "Graham Chapman"
+        },
+		{
+            "quote": "Kilimanjaro is a pretty tricky climb you know, most of it's up until you reach the very very top, and then it tends to slope away rather sharply.",
+            "author": "Graham Chapman"
+        },
+		{
+            "quote": "You know, there are many people in the country today who, through no fault of their own, are sane. Some of them were born sane. Some of them became sane later in their lives.",
+            "author": "Graham Chapman"
+        },
+		{
+            "quote": "Exploding is a perfectly normal medical phenomenon.",
+            "author": "Graham Chapman"
+        },
+		{
+            "quote": "Balance every thought with its opposition. Because the marriage of them is the destruction of illusion.",
+            "author": "Aleister Crowley"
+        },
+		{
+            "quote": "Love is the only principle which makes life tolerable.",
+            "author": "Aleister Crowley"
+        },
+		{
+            "quote": "The customer is usually wrong, but statistics indicate that it doesn't pay to tell him so",
+            "author": "Aleister Crowley"
+        },
+		{
+            "quote": "Tell the truth, but lead so improbable a life that the truth will never be believed.",
+            "author": "Aleister Crowley"
+        },
+		{
+            "quote": "When man lives under government, he is fallen, his worth is gone, and his nature tarnished.",
+            "author": "Adam Weishaupt"
+        },
+		{
+            "quote": "Darken your room, shut the door, empty your mind. You are still in great company.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "In a universe that defies description, all systems of belief can only be false.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "The more Chaotic I am, the more complete I am.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "Embrace reality by imagination.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "And remember, you shall suffer all things and again suffer: until you have sufficient sufferance to accept all things.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "Great thoughts are against all doctrines of conformity",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "Give me the death of my soul. Intoxicate me with self-love.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "The three heads of God were sundered from existence. Who shall say that this was accident? And likewise, who shall say that accidents be not but arabesques within some wider figure? Of this nothing is known.",
+            "author": "Austin Osman Spare"
+        },
+		{
+            "quote": "Tis an ill wind that blows no minds.",
+            "author": "Malaclypse the Younger"
+        },
+		{
+            "quote": "The human race will begin solving its problems on the day that it ceases taking itself so seriously.",
+            "author": "Malaclypse the Younger"
+        },
+		{
+            "quote": "I don’t know man, I didn’t do it.",
+            "author": "Malaclypse the Younger"
+        },
+		{
+            "quote": "There will come a time when it isn't 'They're spying on me through my phone' anymore. Eventually, it will be 'My phone is spying on me'.",
+            "author": "Philip K. Dick"
+        },
+		{
+            "quote": "There is no route out of the maze. The maze shifts as you move through it, because it is alive.",
+            "author": "Philip K. Dick"
+        },
+		{
+            "quote": "I'm not much but I'm all I have.",
+            "author": "Philip K. Dick"
+        },
+		{
+            "quote": "Each of us assumes everyone else knows what he is doing. They all assume we know what we are doing. We don't.",
+            "author": "Philip K. Dick"
+        },
+		{
+            "quote": "To fight the Empire is to be infected by its derangement.... Whoever defeats the Empire becomes the Empire; it proliferates like a virus.",
+            "author": "Philip K. Dick"
+        },
+		{
+            "quote": "I felt myself on the edge of the world; peering over the rim into a fathomless chaos of eternal night.",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "Who knows the end? What has risen may sink, and what has sunk may rise.",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "Our brains deliberately make us forget things, to prevent insanity",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "The world is indeed comic, but the joke is on mankind.",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "I like coffee exceedingly.",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "For I have always been a seeker, a dreamer, and a ponderer on seeking and dreaming.",
+            "author": "H. P. Lovecraft"
+        },
+		{
+            "quote": "Our journey is about being more deeply involved in life, and yet less attached to it.",
+            "author": "Ram Dass"
+        },
+		{
+            "quote": "We're all just walking each other home.",
+            "author": "Ram Dass"
+        },
+		{
+            "quote": "The game is not about becoming somebody, it's about becoming nobody.",
+            "author": "Ram Dass"
+        },
+		{
+            "quote": "Don’t compare your path with anybody else’s. Your path is unique to you.",
+            "author": "Ram Dass"
+        },
+		{
+            "quote": "We're sitting under the tree of our thinking minds, wondering why we're not getting any sunshine!",
+            "author": "Ram Dass"
+        },
+		{
+            "quote": "The perfect fascist state needs to operate in conditions of perpetual warfare. Have you ever noticed how the world has been in constant crisis since World War II?",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "Reality and unreality have no clear distinction in our present circumstances.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "The interior of our skulls contains a portal to infinity.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "We love our superheroes because they refuse to give up on us. We can analyze them out of existence, kill them, ban them, mock them, and still they return, patiently reminding us of who we are and what we wish we could be.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "Life doesn't have plots and subplots and denouements. It's just a big collection of loose ends and dangling threads that never get explained.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "There's a palace in your head, boy. Learn to live in it always.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "Sometimes you wonder, in an interconnected universe, who's dreaming who?",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "It's so horrible to realize you're just the same as everyone else, isn't it?",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "Only nothing is impossible.",
+            "author": "Grant Morrison"
+        },
+		{
+            "quote": "If you inquire into the hurt, you'll know what you're experiencing is love.",
+            "author": "Duncan Trussell"
+        },
+		{
+            "quote": "We've just barely stopped being monkeys.",
+            "author": "Duncan Trussell"
+        },
+		{
+            "quote": "What you're saying is often less important than what you're feeling when you're saying it.",
+            "author": "Duncan Trussell"
+        },
+		{
+            "quote": "Aspire to be the man you pretend to be when you're trying to get laid.",
+            "author": "Joe Rogan"
+        },
+		{
+            "quote": "If you ever start taking things too seriously, just remember that we are talking monkeys on an organic spaceship flying through the universe.",
+            "author": "Joe Rogan"
+        },
+		{
+            "quote": "Greatness and madness are next door neighbours; and they borrow each other's sugar. You don't get there without the other.",
+            "author": "Joe Rogan"
+        },
+		
 ];
